fc13c0ee9d5c67fb1a7e6c5fe36fcdb102c2d591
baczek <m.baczek@satrevolution.com>
2020-10-02 15:40:03 +0200

    ADDED: candy theme

---
+++ candy_theme.h
@@ -0,0 +1,15 @@
+
+static const char col_red[]        = "#ff220c";
+
+static const char col_blue[]        = "#067BC2";
+static const char col_gray[]        = "#a8a8a8";
+
+
+
+static const char col_yellow[]        = "#eac640";
+static const char col_pink[]          = "#d53a98";
+static const char col_cyan[]          = "#36e9e8";
+static const char col_green[]	      = "#36e96e";
+
+static const char col_white[]	      = "#e3e3e3";
+static const char col_black[]	      = "#202025";
